09ea90e8153d3eb048a57cbab8bcc3853fc82046
Satvik Virmani <70750314+satvikvirmani@users.noreply.github.com>
2021-10-02 12:36:20 +0530

    Upgrade: random vowels support

--- example/index.js
+++ example/index.js
@@ -1,5 +1,14 @@
 // Dependencies
 var Vowels = require("../lib");
 
-console.log(Vowels);
+console.log(Vowels());
 // => [ 'a', 'e', 'i', 'o', 'u' ]
+
+console.log(Vowels(false));
+// => [ 'a', 'e', 'i', 'o', 'u' ]
+
+console.log(Vowels(true,3));
+// Array of 3 random vowels
+
+console.log(Vowels(true,8));
+// Array of 8 random vowels
